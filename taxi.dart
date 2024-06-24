05d9f6e247bb68ff189247b5f986f64a1512d3df
peammawat <peammawat.thongpat@gmail.com>
2024-06-24 18:02:22 +0700

    Update taxi.dart

--- taxi.dart
+++ taxi.dart
@@ -9,8 +9,10 @@ void main() {
 
   if (decimalPart < 0.5) {
     km = km.floorToDouble();
+    print(km);
   } else {
     km = km.ceilToDouble();
+    print(km);
   }
 
   if (km <= 10) {
@@ -21,16 +23,16 @@ void main() {
     double result = 98.5 + (7.00 * (km - 10));
     stdout.write("ราคา = $result บาท");
   } else if (km <= 40) {
-    double result = 168.5 + (8.00 * (km - 20));
+    double result = 238.5 + (8.00 * (km - 20));
     stdout.write("ราคา = $result บาท");
   } else if (km <= 60) {
-    double result = 248.5 + (8.50 * (km - 40));
+    double result = 398.5 + (8.50 * (km - 40));
     stdout.write("ราคา = $result บาท");
   } else if (km <= 80) {
-    double result = 333.5 + (9.00 * (km - 60));
+    double result = 568.5 + (9.00 * (km - 60));
     stdout.write("ราคา = $result บาท");
   } else {
-    double result = 423.5 + (10.50 * (km - 80));
+    double result = 748.5 + (10.50 * (km - 80));
     stdout.write("ราคา = $result บาท");
   }
 }
